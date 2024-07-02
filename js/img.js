79c3dc3963b0d9999361a68de74ffabaf66c3fa7
Alison Adriel <164245040+alisonadriel@users.noreply.github.com>
2024-07-02 16:28:11 -0300

    fix: adding comments

--- js/img.js
+++ js/img.js
@@ -1,3 +1,4 @@
+// Arrays contendo os caminhos das imagens antigas e novas
 const oldImages = [
   "img/orange-cat3.jpg",
   "img/orange-cat2.jpg",
@@ -6,31 +7,39 @@ const oldImages = [
 
 const newImages = ["img/catjs3.jpg", "img/catjs2.jpg", "img/catjs1.jpg"];
 
+// Variáveis para rastrear as imagens atuais e as próximas
 let currentImages = oldImages;
 let nextImages = newImages;
 
+// Função para trocar as imagens
 function changeImages() {
   const imgElements = document.querySelectorAll(".cat-img img");
 
+  // Remove a classe 'active' de todas as imagens
   imgElements.forEach((img) => {
     img.classList.remove("active");
   });
 
   setTimeout(() => {
+    // Atualiza o src das imagens
     imgElements.forEach((img, imgIndex) => {
       img.src = nextImages[imgIndex];
     });
 
+    // Adiciona a classe 'active' a todas as imagens
     setTimeout(() => {
       imgElements.forEach((img) => {
         img.classList.add("active");
       });
     }, 50);
 
+    // Troca os arrays currentImages e nextImages
     [currentImages, nextImages] = [nextImages, currentImages];
   }, 1000);
 }
 
+// Troca as imagens a cada 4 segundos
 setInterval(changeImages, 4000);
 
+// Chama a função changeImages inicialmente para definir as imagens
 changeImages();
--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,24 @@
+// Seleciona o botão do menu pelo ID
 const btnMenu = document.getElementById("btn");
 
+// Função para alternar o menu
 function toggleMenu(event) {
+  // Previne o comportamento padrão no evento touchstart
   if (event.type === "touchstart") event.preventDefault();
+
+  // Seleciona o elemento de navegação pelo ID
   const nav = document.getElementById("nav");
+  
+  // Alterna a classe 'active' no elemento de navegação
   nav.classList.toggle("active");
+
+  // Verifica se o menu está ativo
   const active = nav.classList.contains("active");
+  
+  // Define o atributo 'aria-expanded' com base no estado do menu
   event.currentTarget.setAttribute("aria-expanded", active);
+
+  // Atualiza o rótulo 'aria-label' com base no estado do menu
   if (active) {
     event.currentTarget.setAttribute("aria-label", "Fechar Menu");
   } else {
@@ -13,7 +26,6 @@ function toggleMenu(event) {
   }
 }
 
+// Adiciona eventos de clique e toque ao botão do menu
 btnMenu.addEventListener("click", toggleMenu);
 btnMenu.addEventListener("touchstart", toggleMenu);
-
-
