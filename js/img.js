466f5a43985660535278de9f13d34e25488f36dc
Alison Adriel <adriel.dias15@gmail.com>
2024-07-01 23:04:09 -0300

    fix: media-query

--- js/img.js
+++ js/img.js
@@ -1,45 +1,36 @@
 const oldImages = [
   "img/orange-cat3.jpg",
   "img/orange-cat2.jpg",
-  "img/catt4.jpg"
+  "img/catt4.jpg",
 ];
 
-const newImages = [
-  "img/catjs3.jpg",
-  "img/catjs2.jpg",
-  "img/catjs1.jpg"
-];
+const newImages = ["img/catjs3.jpg", "img/catjs2.jpg", "img/catjs1.jpg"];
 
-let currentImages = oldImages; 
-let nextImages = newImages; 
+let currentImages = oldImages;
+let nextImages = newImages;
 
 function changeImages() {
   const imgElements = document.querySelectorAll(".cat-img img");
 
-  
-  imgElements.forEach(img => {
-    img.classList.remove('active');
+  imgElements.forEach((img) => {
+    img.classList.remove("active");
   });
 
-  
   setTimeout(() => {
     imgElements.forEach((img, imgIndex) => {
       img.src = nextImages[imgIndex];
     });
 
-    
     setTimeout(() => {
-      imgElements.forEach(img => {
-        img.classList.add('active');
+      imgElements.forEach((img) => {
+        img.classList.add("active");
       });
-    }, 50); 
-    
+    }, 50);
+
     [currentImages, nextImages] = [nextImages, currentImages];
-  }, 1000); 
+  }, 1000);
 }
 
-
 setInterval(changeImages, 4000);
-
 
 changeImages();
